d3e92d6409dc77ae45db577e6f0fddd99a2ae011
Gabrielzg1 <gabrielzg173@gmail.com>
2023-03-04 17:51:19 -0300

    inicial ideias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from "./logo.svg";
+import React, { useState, useContext } from "react";
 import "./App.css";
 
 function App() {
+	const [algorithm, setAlgorithm] = useState('')
+	const handleTime = async () => {
+		console.log(algorithm)
+	}
+
 	return (
 		<div className="App">
-			<h1>Aqui vai o Código do frontend</h1>
+			<select value={algorithm} onChange={e => setAlgorithm(e.target.value)}>
+				<option value="radix">Radix Sort</option>
+				<option value="counting">Counting Sort</option>
+				<option value="bubble">Bubble Sort</option>
+
+				<option value="binary">Binary Search</option>
+				<option value="linear">Linear Search</option>
+			</select>
+			<div className="actions">
+				<button onClick={handleTime}>Entrar</button>
+			</div>
 		</div>
 	);
 }
